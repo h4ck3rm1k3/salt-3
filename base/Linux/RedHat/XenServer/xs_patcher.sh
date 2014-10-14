15539d8c5ea2eefe39a9f87120dd32cb97ad6c25
root <root@salt.hdtr.com>
2014-10-14 19:06:49 +0800

    update for xs_patcher

--- base/Linux/RedHat/XenServer/xs_patcher.sh
+++ base/Linux/RedHat/XenServer/xs_patcher.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 mount -t nfs 192.168.11.33:/vol/ftp01 /mnt
 cp /mnt/isos/hypervisor/xenserver/xenserver-cloud-supp.iso /tmp
-xe-install-supplemental-pack /tmp/xenserver-cloud-supp.iso > /dev/null
+xe-install-supplemental-pack /tmp/xenserver-cloud-supp.iso
 cd /mnt/updates/xs_patcher
-./xs_patcher.sh > /dev/null &
+./xs_patcher.sh
 sleep 5
 wait
 cd / && umount /mnt
