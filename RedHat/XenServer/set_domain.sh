4f846252ce537bdb59defd47408d771215ac03e3
root <root@salt.hdtr.com>
2014-11-28 16:57:30 +0800

    add "domain hdtr.com" to /etc/resolv.conf for XenServer

--- RedHat/XenServer/set_domain.sh
+++ RedHat/XenServer/set_domain.sh
@@ -3,3 +3,4 @@
 host_name=`hostname`
 pif_mgmt_id=$(xe pif-list host-name-label=$host_name management=true --minimal)
 xe pif-param-set uuid=$pif_mgmt_id other-config:domain=hdtr.com
+grep -q 'hdtr.com' /etc/resolv.conf || echo 'domain hdtr.com' >> /etc/resolv.conf
